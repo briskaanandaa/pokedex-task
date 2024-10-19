af179f11c5bf05e0a34a2277a2d8d2797b48c51c
briskaanandaa <briskaananda103@gmail.com>
2024-10-19 12:24:09 +0700

    first commit

---
+++ backend/generateJsonDB.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const { types } = require("util");
+
+async function generateJsonDB() {
+  try {
+    // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
+    // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
+    // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
+    // pokemon yang telah kalian parsing dari public api pokemon
+    const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
+    const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
+    const payLoad = [];
+    console.log(pokemonList);
+    for (let i = 0; i < 1; i++) {
+      const pokemon = pokemonList.results[i];
+      const detail = await fetch(pokemon.url).then((res) => res.json());
+      const species = await fetch(detail.species.url).then((res) => res.json());
+      const evo = await fetch(species.evolution_chain.url).then((res) =>
+        res.json()
+      );
+
+      const evolutionChains = [evo.chain.species.name];
+      let evolveTo = evo.chain.evolves_to[0];
+      while (evolveTo) {
+        evolutionChains.push(evolveTo.species.name);
+        evolveTo = evolveTo.evolves_to[0];
+      }
+
+      const item = {
+        id: detail.id,
+        name: pokemon.name,
+        height: detail.height,
+        weight: detail.weight,
+        cries: detail.cries,
+        abilities: detail.abilities.map((ab) => ab.ability.name),
+        types: detail.types.map((ab) => ab.type.name),
+        evolutionChains,
+      };
+      payLoad.push(item);
+      console.log(detail);
+    }
+
+    console.log(payLoad);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+generateJsonDB();
